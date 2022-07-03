1f6cd2e5283b647dca5dea064e21f2fc4eb5af3f
ClaudioScappatura <61538901+ClaudioScappatura@users.noreply.github.com>
2022-07-04 01:24:20 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 URL_PROF = "https://www.uniba.it/ricerca/dipartimenti/informatica/dipartimento/personale/docenti-2"
+URL_BIBLIO = "https://www.uniba.it/bibliotechecentri/informatica/biblioteca-di-informatica"
 
 
 def parsing_html(url):
@@ -15,6 +16,26 @@ def parsing_html(url):
     response_html = response.text
     return BeautifulSoup(response_html, 'html.parser')
 
+def library_scraping(url):
+    fulfillmentText = ""
+    soup = parsing_html(url)
+    regex = None
+
+    # il testo viene spaziato e viene dato l'accapo secondo la pagina html ottenuta
+    fulfillmentText = soup.getText(separator=u'\n')
+
+    # matcha solo il testo di interesse per la biblioteca
+    regex = re.search("(?sm)Dove siamo(.*?)(?=[\r\n]*\w*Catalogo)", fulfillmentText, re.IGNORECASE)
+
+    if regex:
+        # formattazione varia
+        fulfillmentText = regex.group(0)
+        fulfillmentText = re.sub("(?sm)“(.*?)Alberto Manguel", "", fulfillmentText)
+        fulfillmentText = re.sub("SERVIZI BIBLIOTECARI", "\nSERVIZI BIBLIOTECARI", fulfillmentText)
+        fulfillmentText = re.sub("MISURE DI SICUREZZA", "\nMISURE DI SICUREZZA", fulfillmentText)
+        fulfillmentText = re.sub("Referente di Biblioteca:", "\nReferente di Biblioteca", fulfillmentText)
+        fulfillmentText = re.sub("Coordinatore Scientifico:", "\nCoordinatore Scientifico:", fulfillmentText)
+    return fulfillmentText
 
 @app.route("/webhooks", methods=["POST"])
 def webhooks():
@@ -25,7 +46,7 @@ def webhooks():
     query_result = req.get('queryResult')
 
     if query_result.get("intent").get("displayName") == "Biblioteca":
-        fulfillmentText = "La biblioteca signoraaa"
+        fulfillmentText = library_scraping(URL_BIBLIO)
 
     # se c'è una risposta, la ritorno sempre in formato JSON
     return {
