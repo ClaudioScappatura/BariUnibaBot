05353376a2c6cf189915a0e29db151c7722d7d68
ClaudioScappatura <61538901+ClaudioScappatura@users.noreply.github.com>
2022-07-07 14:27:06 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -698,12 +698,7 @@ def webhooks():
         fulfillmentText = job_placement_scraping(URL_RESOURCES)
 
     elif query_result.get("intent").get("displayName") == "CDI":
-        if query_result["parameters"]["caratteristicheCDI"]:
-            fulfillmentText = "trovatoCaratteristicheCDI"
-        elif query_result["parameters"]["RichiestaCDI"]:
-            fulfillmentText = "trovatoRichestaCDI"
-        else:
-            fulfillmentText = cie_scraping(URL_CIE, None)
+        fulfillmentText = cie_scraping(URL_CIE, None)
 
     # if fulfillmentText == "":
     #    fulfillmentText = "Ho ancora tanto da imparare, puoi ripetere?"
