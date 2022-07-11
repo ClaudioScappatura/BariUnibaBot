4e0ae6eb7bee928253c484e02b7b183984ee19b3
ClaudioScappatura <61538901+ClaudioScappatura@users.noreply.github.com>
2022-07-11 11:21:01 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -172,27 +172,172 @@ def cie_scraping(url, text, context):
                           "richiedere la CIE (procedimento)\n - Documenti necessari per richiesta CIE\n - " \
                           "Richiedere un duplicato della CIE\n - CIE per cittadini NON comunitari\n - Validità della " \
                           "CIE per l'espartrio\n - Info su PIN e PUK\n - Esperimersi sulla donazione degli    " \
-                          "organi\n - " \ 
+                          "organi\n - " \
                           "Portale CIE\n - Costi della CIE\n - Come pagare la CIE\n - Tempi di arrivo CIE\n - Durata " \
                           "validità CIE "
 
     if text is not None:
         if text == "UFFICIO ANAGRAFE CENTRALE":
             fulfillmentText = "UFFICIO ANAGRAFE CENTRALE - CARTE D'IDENTITÀ\n\nNumero di telefono:\n080/5773357 - " \
-                                  "3304 - 3782\nNumero di Email PEC:\n " \
-                                  "ci.anagrafe.comunebari@pec.rupar.puglia.it\n\nORARI DI APERTURA AL " \
-                                  "PUBBLICO:\n\nLunedì: 9.00 - 12.00\nMartedì: 9.00 - 12.00\nMercoledì: 9.00 - " \
-                                  "12.00\nGiovedì: 9.00 - 12.00 e 15.30 - 17.00\nVenerdì: 9.00 - 12.00\nSabato: " \
-                                  "chiuso\n\nIndirizzo: Corso Vittorio Veneto,4 70122 Bari "
+                              "3304 - 3782\nNumero di Email PEC:\n " \
+                              "ci.anagrafe.comunebari@pec.rupar.puglia.it\n\nORARI DI APERTURA AL " \
+                              "PUBBLICO:\n\nLunedì: 9.00 - 12.00\nMartedì: 9.00 - 12.00\nMercoledì: 9.00 - " \
+                              "12.00\nGiovedì: 9.00 - 12.00 e 15.30 - 17.00\nVenerdì: 9.00 - 12.00\nSabato: " \
+                              "chiuso\n\nIndirizzo: Corso Vittorio Veneto,4 70122 Bari "
         elif text == "UFFICIO ANAGRAFE SAN PASQUALE":
             fulfillmentText = "UFFICIO ANAGRAFE/STATO CIVILE DECENTRATO - DELEGAZIONE CARRASSI-SAN " \
-                                  "PASQUALE\n\nNumero di telefono :  \n080/5772491 080/5772493  080/5772496  " \
-                                  "080/5772497\nNumero di Email PEC " \
-                                  ":\ndelegazione.carrassi.comunebari@pec.rupar.puglia.it\n\nPosta elettronica :\n " \
-                                  "delegazione.oriente@comune.bari.it\n\nOrari di apertura al pubblico :\nLunedì : " \
-                                  "9.00 - 12.30\nMartedì : 9.00 - 12.30\nMercoledì : 9.00 - 12.30\nGiovedì : 9.00 - " \
-                                  "13.00 e 16.00 - 17.30\nVenerdì : 9.00 - 12.30\nSabato : chiuso\n\nIndirizzo : Via " \
-                                  "Luigi Pinto,3 70125 Bari "
+                              "PASQUALE\n\nNumero di telefono :  \n080/5772491 080/5772493  080/5772496  " \
+                              "080/5772497\nNumero di Email PEC " \
+                              ":\ndelegazione.carrassi.comunebari@pec.rupar.puglia.it\n\nPosta elettronica :\n " \
+                              "delegazione.oriente@comune.bari.it\n\nOrari di apertura al pubblico :\nLunedì : " \
+                              "9.00 - 12.30\nMartedì : 9.00 - 12.30\nMercoledì : 9.00 - 12.30\nGiovedì : 9.00 - " \
+                              "13.00 e 16.00 - 17.30\nVenerdì : 9.00 - 12.30\nSabato : chiuso\n\nIndirizzo : Via " \
+                              "Luigi Pinto,3 70125 Bari "
+
+    return fulfillmentText
+
+
+# scraping sulle info inerenti al cambio di residenza
+def CR_scraping(url, text, context):
+    fulfillmentText = ""
+    soup = parsing_html(url)
+
+    # flag utile per la gestione degli allegati
+    allegati = False
+    if text is not None:
+        # flag per la gestione delle stampe
+        printText = False
+        match text:
+            case "ALLEGARE":
+                text = "DOCUMENTI DA ALLEGARE"
+            case "STRANIERI":
+                text = "CITTADINI STRANIERI"
+            case "MINORI":
+                text = "MINOR"
+    elif context is not None:
+        match context:
+            case "CR_COSA":
+                # COSA del cambio di residenza
+                context = "accordion_descrizione_servizio_11639056"
+            case "CR_COME":
+                # COME del cambio di residenza
+                context = "accordion_come_11639056"
+                printText = True
+            case "CR_DOVE":
+                # DOVE del cambio di residenza
+                context = "accordion_dove_11639056"
+            case "CR_COSTI":
+                # COSTI cambio di resistenza
+                context = "accordion_costi_11639056"
+            case "CR_TEMPI":  # da implementare poiche dinamico
+                # TEMPI del cambio di residenza
+                context = "accordion_tempi_11639056"
+            case "CR_ALLEGATI":
+                allegati = True
+    else:  # se text e context sono nulli, stampa tutte le informazioni in pagina
+        printText = True
+
+    if allegati is False:
+        CR_Soup = soup.findAll('div', class_="accordion-body collapse in")
+        # se la variabile text è vuota, stampa tutte le info riguardo la CDI, altrimenti
+        # ricerca tutti i DIV di quella classe
+        for i in CR_Soup:
+            if context is not None:
+                if i["id"] == context:
+                    printText = True
+                else:
+                    printText = False
+            try:
+                # analizza il primo figlio di ogni DIV trovato (il primo figlio sarà un altro DIV
+                for t in i.children:
+                    try:
+                        # è la lista dei figli del secondo DIV
+                        for k in t.children:
+                            if k.name is not None:
+                                # verifica sul numero di figli
+                                has_child = len(k.findAll()) != 0
+                                # se il tag ha figli allora cerca e stampa i figli
+                                if has_child:
+                                    # è la lista di figli di ogni TAG all'interno di DIV
+                                    for z in k.children:
+                                        if z.name is not None:
+                                            # se è una scritta in stampatello senza tag che la precedono allora la stampo, la tolgo dal tag superiore, stampo il testo del tag padre e vado a capo
+                                            if z.text.isupper() and len(
+                                                    z.findPreviousSiblings()) == 0 and z.text != "N.B.":
+                                                if context is None:
+                                                    if text is not None:
+                                                        if text in z.text:
+                                                            printText = True
+                                                        else:
+                                                            printText = False
+                                                    else:
+                                                        printText = True
+                                                if printText is True:
+                                                    fulfillmentText += "\n"
+                                                    fulfillmentText += z.text
+                                                    # se il titolo in maiuscolo contiene fratelli all'interno dello stesso TAG, allora mette uno spazio per andare a capo
+                                                    if len(z.findNextSiblings()) != 0:
+                                                        fulfillmentText += "\n"
+                                                    fulfillmentText += k.text.replace(str(z.text), "")
+                                                    fulfillmentText += "\n"
+                                                    break  # esco per evitare duplicati
+
+                                            # se il tag è 'li' (elenco puntato), allora metti un a capo
+                                            elif z.name == "li" and printText is True:
+                                                fulfillmentText += "- "
+                                                fulfillmentText += z.text
+                                                fulfillmentText += "\n"
+                                            # se il tag non ha fratelli precedenti e successivi, allora stampa prima il testo del padre e poi il proprio (ESCLUSIONE DUPLICATI)
+                                            elif (len(z.findPreviousSiblings()) == 0 and len(
+                                                    z.findNextSiblings()) == 0):
+                                                if printText is True:
+                                                    fulfillmentText += k.text.replace(str(z.text), "")
+                                                    fulfillmentText += z.text
+                                                    fulfillmentText += "\n"
+                                            else:  # se il tag ha più figli che però non rientrano in uno dei casi particolari precedenti, allora stampa il padre
+                                                if printText is True:
+                                                    fulfillmentText += k.text
+                                                    fulfillmentText += "\n"
+                                                    break  # evita duplicati
+
+
+                                # se il tag non ha figli, stampa il suo testo
+                                else:
+                                    if printText is True:
+                                        fulfillmentText += k.text
+                                        fulfillmentText += "\n"
+                    except:
+                        continue
+            except:
+                continue
+    else:
+        # stampa tutti i dati inerenti ai moduli per cambio di residenza
+        CR_Soup = soup.findAll('a', class_="inverted-link")
+        for links in CR_Soup:
+            print(links.text + ": \n - " + "https://www.comune.bari.it" + links["href"] + "\n")
+
+    if text is not None:
+        if text == "UFFICIO ANAGRAFE CENTRALE":
+            fulfillmentText = "UFFICIO ANAGRAFE CENTRALE - DICHIARAZIONI DI RESIDENZA E CAMBI DI DOMICILIO\n\nNumero " \
+                              "di telefono:\n080/5773332 - 3333 - 3355 - 3376 - 3344 - 3314 - 3729 - 6450 - 2489 - " \
+                              "4636 - 4606\nNumero di fax:\n080/5773359\nNumero di Email " \
+                              "PEC:\nanagrafe.comunebari@pec.rupar.puglia.it\nPosta elettronica " \
+                              ":\nufficio.dichiarazioniresidenza@comune.bari.it\n\nIndirizzo : Corso Vittorio Veneto," \
+                              "4 70122 Bari "
+
+    if context == "accordion_come_11639056":
+        match1 = re.search("(?sm)al fine di (.*?)(?=[\r\n]*\w*2\) TRASFERIMENTO DI NUCLEO)",
+                           fulfillmentText, re.IGNORECASE)
+        fulfillmentText = match1.group(0)
+
+    if context == "accordion_costi_11639056":
+        fulfillmentText = "COSTO CAMBIO DI RESIDENZA:\nGratuito"
+
+    if context == "INFO":
+        fulfillmentText = "Cosa vuoi sapere nello specifico?\n - Cos'è il C.D.R.(cambio di residenza)\n - Come " \
+                          "cambiare residenza\n - Documenti da allegare al C.D.R.\n - Cambio residenza cittadini " \
+                          "stranieri\n - Costo del C.D.R.\n - Costo del C.D.R.\n - Tempi necessari per il C.D.R.\n - " \
+                          "Moduli per il C.D.R.\n - Orari di apertura ufficio anagrafe "
 
     return fulfillmentText
 
@@ -274,7 +419,6 @@ def webhooks():
         fulfillmentText = cie_scraping(URL_CIE, "COSTI", None)
     elif query_result.get("intent").get("displayName") == "CIE_PAGAMENTO":
         fulfillmentText = cie_scraping(URL_CIE, "PAGAMENTO", None)
-
 
     # if fulfillmentText == "":
     #    fulfillmentText = "Ho ancora tanto da imparare, puoi ripetere?"
